91b0709187032183d31a777683e3b98a829771cd
ThreeHoolagins <cbcunningham20@gmail.com>
2025-07-02 15:20:08 -0600

    updates to use a "business" email for sending

--- emailPage.py
+++ emailPage.py
@@ -3,12 +3,12 @@ import smtplib
 from email.message import EmailMessage
 import traceback
 
-from data import PERSONAL_EMAIL, PERSONAL_EMAIL_APP_PASSWORD
+from data import PROJECT_EMAIL, PERSONAL_EMAIL, PROJECT_EMAIL_APP_PASSWORD
 
 def generateMessage(exception_message):
     msg = EmailMessage()
     msg["Subject"] = "Ranked Race Job Failed"
-    msg["From"] = PERSONAL_EMAIL
+    msg["From"] = PROJECT_EMAIL
     msg["To"] = PERSONAL_EMAIL
     content = "The Ranked Race Has Failed. See Below. \n\n"
     content += exception_message
@@ -21,7 +21,7 @@ def PageError(exception_message):
     
     try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(PERSONAL_EMAIL, PERSONAL_EMAIL_APP_PASSWORD)
+            smtp.login(PROJECT_EMAIL, PROJECT_EMAIL_APP_PASSWORD)
             smtp.send_message(msg)
     except Exception as e:
         LOG.error(traceback.format_exc())
