a461b8f146b4fc3fd71ed41224895c318f3f08ea
ThreeHoolagins <60825956+ThreeHoolagins@users.noreply.github.com>
2025-09-04 19:13:26 -0600

    Create example data.py

---
+++ data.py
@@ -0,0 +1,28 @@
+# Edit these values to fit your group and your api keys as needed.
+
+# This is two lists with corresponding Names and Tag lines
+# From the league format Name#Tag
+# The Indecies should match to the same person.
+FRIENDS_GAME_NAMES = ["Name1",
+                      "Name2",
+                      "Name3"]
+FRIENDS_TAG_LINE   = ["Tag1",
+                      "Tag2",
+                      "Tag3"]
+
+# The channel to post the ranked summary in
+DISCORD_CHANNEL_ID = "XXXXX"
+# The channel to post the patches to
+NEWS_CHANNEL_ID = "XXXXX"
+
+# Used for which email to send to
+PERSONAL_EMAIL="testEmail@domain.com"
+
+# Used for which email to send from, you need to setup an app password 
+# This is setup for gmail
+PROJECT_EMAIL="testEmail@domain.com"
+PROJECT_EMAIL_APP_PASSWORD="XXXX XXXX XXXX XXXX"
+
+# API Keys
+RIOT_API_KEY = "RGAPI-XXXX-XXXXX-XXXX-XXXXX"
+DISCORD_BOT_KEY = 'Bot XXXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXX'
